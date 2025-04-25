ec425a9f01cca3b08ebbae32875284b92eb356ac
rohingilman <rohingilman@gmail.com>
2025-04-25 21:13:47 +0000

    Deploying to gh-pages from @ mcmcseminar/mcmcseminar.github.io@b03ff400dc5b7507c44d27c14d853347635d9dd4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-home",
-          title: "home",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-about",
+  },{id: "nav-about",
           title: "about",
           description: "",
           section: "Navigation",
